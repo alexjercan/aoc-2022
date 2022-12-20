73500e10ac35a25dac45eb83ec65eb853919bda8
Alex Jercan <jercan_alex27@yahoo.com>
2022-12-20 19:54:19 +0200

    format

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -18,47 +18,41 @@ fn mix(xs: &mut Vec<(usize, isize)>, order: &Vec<(usize, isize)>) {
 }
 
 fn part1(input: &Vec<isize>) -> String {
-    let mut xs: Vec<(isize, bool)> = input.into_iter().map(|x| (*x, false)).collect();
+    let input: Vec<(usize, isize)> = input.into_iter().enumerate().map(|x| (x.0, *x.1)).collect();
+    let mut xs = input.clone();
 
-    let mut i = 0;
+    mix(&mut xs, &input);
 
-    while i < xs.len() {
-        match xs[i] {
-            (_, true) => i = i + 1,
-            (x, false) => {
-                let im = (i as isize + x).rem_euclid((xs.len() - 1) as isize) as usize;
-
-                xs.remove(i);
-                xs.insert(im, (x, true));
-
-                if im < i {
-                    i = i + 1;
-                }
-            }
-        }
-    }
-
-    let xs = xs.iter().map(|(x, _)| *x).collect::<Vec<isize>>();
-    let zero = xs.iter().position(|x| *x == 0).expect("to have at least one zero in input");
-
-    return [1000, 2000, 3000].iter().fold(0, |acc, i| {
-            acc + xs[(zero + i).rem_euclid(xs.len())]
-    }).to_string();
+    let zero = xs
+        .iter()
+        .position(|x| (*x).1 == 0)
+        .expect("to have at least one zero in input");
+    return [1000, 2000, 3000]
+        .iter()
+        .fold(0, |acc, i| acc + xs[(zero + i).rem_euclid(xs.len())].1)
+        .to_string();
 }
 
 fn part2(input: &Vec<isize>) -> String {
-    let input: Vec<(usize, isize)> = input.into_iter().enumerate().map(|x| (x.0, x.1 * 811589153)).collect();
+    let input: Vec<(usize, isize)> = input
+        .into_iter()
+        .enumerate()
+        .map(|x| (x.0, x.1 * 811589153))
+        .collect();
     let mut xs = input.clone();
 
     for _ in 0..10 {
         mix(&mut xs, &input);
     }
 
-    let zero = xs.iter().position(|x| (*x).1 == 0).expect("to have at least one zero in input");
-
-    return [1000, 2000, 3000].iter().fold(0, |acc, i| {
-            acc + xs[(zero + i).rem_euclid(xs.len())].1
-    }).to_string();
+    let zero = xs
+        .iter()
+        .position(|x| (*x).1 == 0)
+        .expect("to have at least one zero in input");
+    return [1000, 2000, 3000]
+        .iter()
+        .fold(0, |acc, i| acc + xs[(zero + i).rem_euclid(xs.len())].1)
+        .to_string();
 }
 
 fn main() {
@@ -72,26 +66,30 @@ fn main() {
 mod tests {
     #[test]
     fn part1_example1() {
-        let input = super::parse_input("1
+        let input = super::parse_input(
+            "1
 2
 -3
 3
 -2
 0
-4");
+4",
+        );
 
         assert_eq!(super::part1(&input), "3");
     }
 
     #[test]
     fn part2_example1() {
-        let input = super::parse_input("1
+        let input = super::parse_input(
+            "1
 2
 -3
 3
 -2
 0
-4");
+4",
+        );
 
         assert_eq!(super::part2(&input), "1623178306");
     }
