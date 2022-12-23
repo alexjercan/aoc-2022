9b2668cf0885f6bfa2eb4b1a6d94e5f390f1b302
Alex Jercan <jercan_alex27@yahoo.com>
2022-12-23 11:40:01 +0200

    feat: added day 23

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,6 +1,8 @@
+use std::collections::{HashSet, HashMap};
+
 use glam::{ivec2, IVec2};
 
-fn parse_input(input: impl AsRef<str>) -> Vec<IVec2> {
+fn parse_input(input: impl AsRef<str>) -> HashSet<IVec2> {
     input
         .as_ref()
         .lines()
@@ -25,7 +27,7 @@ enum Dir {
     East,
 }
 
-fn propose(position: IVec2, others: &Vec<IVec2>, dirs: [Dir; 4]) -> Option<IVec2> {
+fn propose(position: IVec2, others: &HashSet<IVec2>, dirs: [Dir; 4]) -> IVec2 {
     let n_positions = [
         position + ivec2(-1, -1),
         position + ivec2(0, -1),
@@ -37,8 +39,8 @@ fn propose(position: IVec2, others: &Vec<IVec2>, dirs: [Dir; 4]) -> Option<IVec2
         position + ivec2(1, 0),
     ];
 
-    if others.iter().all(|pos| !n_positions.contains(pos)) {
-        return None;
+    if n_positions.iter().all(|n_pos| !others.contains(n_pos)) {
+        return position;
     }
 
     for dir in &dirs {
@@ -65,46 +67,36 @@ fn propose(position: IVec2, others: &Vec<IVec2>, dirs: [Dir; 4]) -> Option<IVec2
             ],
         };
 
-        if others.iter().all(|pos| !n_positions.contains(pos)) {
-            return Some(n_positions[1]);
+        if n_positions.iter().all(|n_pos| !others.contains(n_pos)) {
+            return n_positions[1];
         }
     }
 
-    return None;
+    return position;
 }
 
-fn round(elfs: &Vec<IVec2>, dirs: [Dir; 4]) -> Vec<IVec2> {
-    let props: Vec<_> = elfs
-        .iter()
-        .map(|&position| propose(position, elfs, dirs))
-        .collect();
+fn round(elves: &HashSet<IVec2>, dirs: [Dir; 4]) -> HashSet<IVec2> {
+    elves.iter().map(|&elf| {
+        (propose(elf, elves, dirs), elf)
+    }).fold(HashMap::<IVec2, Vec<IVec2>>::new(), |mut acc, (prop, elf)| {
+        acc.entry(prop).and_modify(|e| e.push(elf)).or_insert(vec![elf]);
 
-    return props
-        .iter()
-        .enumerate()
-        .map(|(i, &prop)| match prop {
-            Some(position) => {
-                if props
-                    .iter()
-                    .enumerate()
-                    .all(|(j, prop)| i == j || prop.is_none() || prop.unwrap() != position)
-                {
-                    return position;
-                } else {
-                    return elfs[i];
-                }
-            }
-            None => elfs[i],
-        })
-        .collect();
+        return acc;
+    }).iter().flat_map(|(&prop, elves)| {
+        if elves.len() == 1 {
+            return vec![prop];
+        } else {
+            return elves.to_owned();
+        }
+    }).collect()
 }
 
-fn part1(input: &Vec<IVec2>) -> String {
-    let mut elfs = input.clone();
+fn part1(input: &HashSet<IVec2>) -> String {
+    let mut elves = input.clone();
     let mut dirs = [Dir::North, Dir::South, Dir::West, Dir::East];
 
     for _ in 0..10 {
-        elfs = round(&elfs, dirs);
+        elves = round(&elves, dirs);
 
         let used = dirs[0];
         for i in 0..3 {
@@ -113,7 +105,7 @@ fn part1(input: &Vec<IVec2>) -> String {
         dirs[3] = used;
     }
 
-    let (min_x, max_x) = elfs
+    let (min_x, max_x) = elves
         .iter()
         .fold((i32::MAX, i32::MIN), |(mut min, mut max), elf| {
             min = min.min(elf.x);
@@ -121,7 +113,7 @@ fn part1(input: &Vec<IVec2>) -> String {
 
             return (min, max);
         });
-    let (min_y, max_y) = elfs
+    let (min_y, max_y) = elves
         .iter()
         .fold((i32::MAX, i32::MIN), |(mut min, mut max), elf| {
             min = min.min(elf.y);
@@ -131,11 +123,30 @@ fn part1(input: &Vec<IVec2>) -> String {
         });
 
 
-    return ((max_x - min_x + 1) * (max_y - min_y + 1) - elfs.len() as i32).to_string();
+    return ((max_x - min_x + 1) * (max_y - min_y + 1) - elves.len() as i32).to_string();
 }
 
-fn part2(input: &Vec<IVec2>) -> String {
-    "".to_owned()
+fn part2(input: &HashSet<IVec2>) -> String {
+    let mut elves = input.clone();
+    let mut dirs = [Dir::North, Dir::South, Dir::West, Dir::East];
+
+    for index in 1..usize::MAX {
+        let n_elves = round(&elves, dirs);
+
+        if n_elves == elves {
+            return index.to_string();
+        }
+
+        let used = dirs[0];
+        for i in 0..3 {
+            dirs[i] = dirs[i+1];
+        }
+        dirs[3] = used;
+
+        elves = n_elves;
+    }
+
+    return "no way you waited this long".to_owned();
 }
 
 fn main() {
